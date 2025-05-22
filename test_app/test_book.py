0aa2afab6f0cdb3cc96f0d683250f57f7d99991a
Nonso-Marshal <anawanacu@gmail.com>
2025-05-22 20:54:47 +0100

    second commit

--- test_app/test_book.py
+++ test_app/test_book.py
@@ -1,15 +1,13 @@
 from fastapi.testclient import TestClient
 from main import app
-from schemas.book import BookCreate
+from schemas.book import BookCreate, BookUpdate
+from uuid import uuid4
 
 client = TestClient(app)
-
 
 def test_get_books():
     response = client.get("/books")
     assert response.status_code == 200
-    assert isinstance(response.json(), dict)
-
 
 def test_add_book():
     payload = {
@@ -21,9 +19,9 @@ def test_add_book():
     }
     response = client.post("/books", json=payload)
     data = response.json()
+    assert response.status_code == 200
     assert data["message"] == "Book added successfully"
-    assert data["data"]["title"] == "Johny bravo"
-
+    assert data["data"]["title"] == payload["title"]
 
 def test_get_book_by_id():
     payload = {
@@ -34,21 +32,15 @@ def test_get_book_by_id():
         "language": "English"
     }
     response = client.post("/books", json=payload)
-    add_book_data = response.json()
-    book_id = add_book_data['data']['id']
+    book_id = response.json()["data"]["id"]
     get_response = client.get(f"/books/{book_id}")
-    get_book_data = get_response.json()
     assert get_response.status_code == 200
-    assert get_book_data['id'] == book_id
-
 
 def test_get_book_by_id_not_found():
-    book_id = 1
+    book_id = str(uuid4())
     get_response = client.get(f"/books/{book_id}")
-    get_book_data = get_response.json()
     assert get_response.status_code == 404
-    assert get_book_data['detail'] == "book not found."
-
+    assert get_response.json()["detail"] == "book not found."
 
 def test_update_book():
     payload = {
@@ -59,8 +51,7 @@ def test_update_book():
         "language": "English"
     }
     response = client.post("/books", json=payload)
-    add_book_data = response.json()
-    book_id = add_book_data['data']['id']
+    book_id = response.json()["data"]["id"]
     update_payload = {
         "title": "Things Fall Apart",
         "author": "Chinua Achebe",
@@ -69,10 +60,10 @@ def test_update_book():
         "language": "Nigerian English"
     }
     update_response = client.put(f"/books/{book_id}", json=update_payload)
-    update_book_data = update_response.json()
     assert update_response.status_code == 200
-    assert update_book_data['message'] == "Book updated successfully"
-    assert update_book_data['data']['title'] == update_payload['title']
+    update_book_data = update_response.json()
+    assert update_book_data["message"] == "Book updated successfully"
+    assert update_book_data["data"]["title"] == update_payload["title"]
 
 def test_update_book_not_found():
     book_id = str(uuid4())
@@ -87,8 +78,6 @@ def test_update_book_not_found():
     assert update_response.status_code == 404
     assert update_response.json()["detail"] == f"Book with id: {book_id} not found"
 
-
-
 def test_delete_book():
     payload = {
         "title": "Johny bravo",
@@ -98,9 +87,14 @@ def test_delete_book():
         "language": "English"
     }
     response = client.post("/books", json=payload)
-    add_book_data = response.json()
-    book_id = add_book_data['data']['id']
+    book_id = response.json()["data"]["id"]
     delete_response = client.delete(f"/books/{book_id}")
-    delete_book_data = delete_response.json()
     assert delete_response.status_code == 200
-    assert delete_book_data['message'] == "Book deleted successfully"
+    delete_book_data = delete_response.json()
+    assert delete_book_data["message"] == "Book deleted successfully"
+
+def test_delete_book_not_found():
+    book_id = str(uuid4())
+    delete_response = client.delete(f"/books/{book_id}")
+    assert delete_response.status_code == 404
+    assert delete_response.json()["detail"] == f"Book with id: {book_id} not found"
